496066daf88e4336d692a662e49968eaa72d659f
Andrew Hundt <ATHundt@gmail.com>
2016-07-25 12:22:57 -0400

    python.sh python setup script added

---
+++ python.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+# source: https://gist.github.com/phatblat/1713458
+# Save script's current directory
+DIR=$(pwd)
+
+# /bin/bash
+set -e
+set -u
+set -x
+
+
+sudo apt install python-setuptools python-dev build-essential
+sudo easy_install pip
+sudo pip install --upgrade virtualenv
+
+
+cd $DIR
