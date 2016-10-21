626ef3a95bfb539437e80acabcf3bb45455f9bef
Andrew Hundt <ATHundt@gmail.com>
2016-10-21 18:28:03 -0400

    added byobu instructions

--- commandline_utils.sh
+++ commandline_utils.sh
@@ -33,7 +33,8 @@ echo ""
 echo "# https://wiki.ubuntu.com/ubuntu-make"
 echo ""
 echo ""
-echo "Installs tmux persistent multiplexed console"
+echo "Installs tmux persistent multiplexed console,"
+echo "so when you lose internet with ssh your programs keep running"
 echo ""
 echo "https://help.ubuntu.com/community/Repositories/Ubuntu"
 
@@ -47,11 +48,22 @@ echo "# byobu        "
 echo "###############"
 echo ""
 echo "tool that makes life with tmux better" 
+echo "see comments of this file for additional setup steps"
 echo ""
 echo "https://www.digitalocean.com/community/tutorials/how-to-install-and-use-byobu-for-terminal-management-on-ubuntu-16-04"
 
 sudo apt install -y byobu
 
+echo ""
+echo "Additional byobu manual setup steps:"
+echo ""
+echo " byobu-enable"
+echo " byobu-select-backend"
+echo "# set default shell to zsh: https://askubuntu.com/questions/296377/how-do-i-change-the-default-shell-used-in-byobu-tmux"
+echo " Put the following in your $HOME/.byobu/.tmux.conf:"
+echo ""
+echo " set -g default-shell /usr/bin/zsh"
+echo " set -g default-command /usr/bin/zsh"
 
 
 cd $DIR
