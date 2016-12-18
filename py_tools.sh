ca89b2318d28e097ca764a27d1e2fd018c7fa5ec
Andrew Hundt <ATHundt@gmail.com>
2016-12-17 23:36:32 -0500

    added jupyter

---
+++ py_tools.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+# source: https://gist.github.com/phatblat/1713458
+# Save script's current directory
+DIR=$(pwd)
+
+# /bin/bash
+set -e
+set -u
+set -x
+
+
+pip install jupyter
+
+
+cd $DIR
