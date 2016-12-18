9b2c0e11df64f3dc6f3aeb08810e3985697940fb
Andrew Hundt <ATHundt@gmail.com>
2016-12-18 02:56:08 -0500

    added ms coco dataset

---
+++ coco.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+# source: https://gist.github.com/phatblat/1713458
+# Save script's current directory
+DIR=$(pwd)
+
+# /bin/bash
+set -e
+set -u
+set -x
+
+mkdir -p ~/datasets
+cd ~/datasets
+
+# Note that by running this download you agree to microsoft's terms and conditions
+# http://mscoco.org/dataset/#download
+curl -O http://msvocds.blob.core.windows.net/coco2014/train2014.zip http://msvocds.blob.core.windows.net/coco2014/val2014.zip http://msvocds.blob.core.windows.net/coco2014/test2014.zip http://msvocds.blob.core.windows.net/coco2015/test2015.zip http://msvocds.blob.core.windows.net/annotations-1-0-3/instances_train-val2014.zip http://msvocds.blob.core.windows.net/annotations-1-0-3/person_keypoints_trainval2014.zip http://msvocds.blob.core.windows.net/annotations-1-0-3/captions_train-val2014.zip http://msvocds.blob.core.windows.net/annotations-1-0-4/image_info_test2014.zip http://msvocds.blob.core.windows.net/annotations-1-0-4/image_info_test2015.zip
+
+
+# install coco python repository git@github.com:pdollar/coco.git
+cd ~/src
+if [ ! -d ~/src/coco ]
+then
+	git clone git@github.com:pdollar/coco.git
+fi
+
+cd ~/src/coco
+git pull
+cd PythonAPI
+make install
+
+cd $DIR
