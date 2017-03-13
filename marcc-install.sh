0839c5ce3398871f08a8bae93c2a0683e3007147
Andrew Hundt <ATHundt@gmail.com>
2017-03-13 18:38:17 -0400

    marcc-install.sh fixes

--- marcc-install.sh
+++ marcc-install.sh
@@ -23,7 +23,7 @@ echo "# to see list of possible modules run:"
 echo "# module avail"
 
 #module load git/2.6.4 boost/1.60.0 cmake/3.5.0 cuda/8.0 cudnn/5.0 python/2.7.12 tensorflow/r1.0 parallel_studio_xe_2015/17.0.1 intel-tbb-oss/intel64/43_20150424oss intel-mpi/2017.1.132
-module load git zsh tmux boost cmake cuda/8.0 cudnn/5.0 python tensorflow parallel_studio_xe_2015 intel-tbb-oss intel-mpi readline glog ruby
+module load git zsh tmux boost cmake cuda/8.0 cudnn/5.0 python tensorflow parallel_studio_xe_2015 intel-mpi readline ruby
 
 # save the set of modules to enable reloading in the future
 save robotics_setup_modules
