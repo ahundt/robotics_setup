7954277bf4408a2905ca4e0a3b5beef1770ef97a
Andrew Hundt <ATHundt@gmail.com>
2017-03-15 13:01:08 -0400

    marcc fix

--- marcc-install.sh
+++ marcc-install.sh
@@ -54,7 +54,7 @@ echo "# https://github.com/jhollowayj/tensorflow_slurm_manager"
 
 # module load git/2.6.4 boost/1.60.0 cmake/3.5.0 cuda/8.0 cudnn/5.0 python/2.7.12 tensorflow/r1.0 parallel_studio_xe_2015/17.0.1 intel-tbb-oss/intel64/43_20150424oss intel-mpi/2017.1.132
 # module load gcc slurm git zsh tmux boost cmake cuda/8.0 cudnn/5.0 python tensorflow parallel_studio_xe_2015 intel-mpi readline ruby
-source marcc-config.sh
+source $DIR/marcc-config.sh
 
 # save the set of modules to enable reloading in the future
 module save robotics_setup_modules
