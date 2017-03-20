f191f298e0c8bfb5f980ab575e02941444e1a5ce
Andrew Hundt <ATHundt@gmail.com>
2017-03-20 08:29:40 -0400

    coco.sh unzip

--- datasets/coco.sh
+++ datasets/coco.sh
@@ -17,14 +17,14 @@ echo " To install supporting software run robotics_setup/coco.sh"
 
 
 if [ ! -d $HOME/datasets ]
-  mkdir -p $HOME/datasets
+  mkdir -p $HOME/datasets/coco
 fi
 
-cd $HOME/datasets
+cd $HOME/datasets/coco
 
 # Note that by running this download you agree to microsoft's terms and conditions
 # http://mscoco.org/dataset/#download
-if [ ! -f $HOME/datasets/train2014.zip ] ; then
+if [ ! -f $HOME/datasets/coco/train2014.zip ] ; then
   curl -O http://msvocds.blob.core.windows.net/coco2014/train2014.zip
   curl -O http://msvocds.blob.core.windows.net/coco2014/val2014.zip
   curl -O http://msvocds.blob.core.windows.net/coco2014/test2014.zip
@@ -34,6 +34,16 @@ if [ ! -f $HOME/datasets/train2014.zip ] ; then
   curl -O http://msvocds.blob.core.windows.net/annotations-1-0-3/captions_train-val2014.zip
   curl -O http://msvocds.blob.core.windows.net/annotations-1-0-4/image_info_test2014.zip
   curl -O http://msvocds.blob.core.windows.net/annotations-1-0-4/image_info_test2015.zip
+
+  unzip -u train2014.zip
+  unzip -u val2014.zip
+  unzip -u test2014.zip
+  unzip -u test2015.zip
+  unzip -u instances_train-val2014.zip
+  unzip -u person_keypoints_trainval2014.zip
+  unzip -u captions_train-val2014.zip
+  unzip -u image_info_test2014.zip
+  unzip -u image_info_test2015.zip
 
 fi
 
