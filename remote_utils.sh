da9b4b79ca26d474e000338b7b2aeddfdd9cd42f
Andrew Hundt <ATHundt@gmail.com>
2017-03-21 23:21:36 -0400

    remote_utils.sh added

---
+++ remote_utils.sh
@@ -0,0 +1,9 @@
+
+# Show images and download files in iTerm2 from remote machine
+# https://www.iterm2.com/documentation-images.html
+
+cd ~/bin
+
+curl -O https://raw.github.com/gnachman/iTerm2/master/tests/imgls
+curl -O https://raw.github.com/gnachman/iTerm2/master/tests/imgcat
+curl -O https://raw.github.com/gnachman/iTerm2/master/tests/download.sh
