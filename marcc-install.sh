e4903719c01fce79a47d51084feb08b208bd21ac
Andrew Hundt <ATHundt@gmail.com>
2017-03-23 15:26:49 -0400

    marcc-install.sh improvement

--- marcc-install.sh
+++ marcc-install.sh
@@ -68,9 +68,6 @@ echo "# https://github.com/jhollowayj/tensorflow_slurm_manager"
 # Command Line Environment Config
 ##################################
 
-
-# module load git/2.6.4 boost/1.60.0 cmake/3.5.0 cuda/8.0 cudnn/5.0 python/2.7.12 tensorflow/r1.0 parallel_studio_xe_2015/17.0.1 intel-tbb-oss/intel64/43_20150424oss intel-mpi/2017.1.132
-# module load gcc slurm git zsh tmux boost cmake cuda/8.0 cudnn/5.0 python tensorflow parallel_studio_xe_2015 intel-mpi readline ruby
 source $DIR/marcc-config.sh
 
 # save the set of modules to enable reloading in the future
@@ -98,6 +95,10 @@ fi
 
 if [ ! -f $HOME/src/.env ]; then
   ln -s .autoenv $HOME/src/.env
+fi
+
+if [ ! -f $HOME/.zshrc ]; then
+  ln -s .zshrc $HOME/.zshrc
 fi
 
 ./zsh.sh
