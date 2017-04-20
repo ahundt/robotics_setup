aa15f4fd04e52e44010421a4b034d360484361d2
Andrew Hundt <ATHundt@gmail.com>
2017-04-20 10:56:15 -0400

    *.sh permissions fix

--- remote_utils.sh
+++ remote_utils.sh
@@ -1,9 +1,14 @@
+set -e
+set -x
 
+DIR=`pwd`
 # Show images and download files in iTerm2 from remote machine
 # https://www.iterm2.com/documentation-images.html
-
+mkdir -p ~/bin
 cd ~/bin
 
 curl -O https://raw.github.com/gnachman/iTerm2/master/tests/imgls
 curl -O https://raw.github.com/gnachman/iTerm2/master/tests/imgcat
 curl -O https://raw.github.com/gnachman/iTerm2/master/tests/download.sh
+
+cd $DIR
