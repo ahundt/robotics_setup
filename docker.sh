49c8654b06dd110b5e6ca85bdfc72ecd6e6f394d
Andrew Hundt <ATHundt@gmail.com>
2017-04-20 10:54:23 -0400

    docker.sh added

---
+++ docker.sh
@@ -0,0 +1,15 @@
+
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo apt-get update
+
+sudo apt-get install docker-ce
