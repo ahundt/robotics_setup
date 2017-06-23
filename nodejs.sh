3be467a234661399c544d0904089aa1bfc14d1ca
Andrew Hundt <ATHundt@gmail.com>
2017-06-23 13:25:33 -0400

    nodejs.sh extra config for ubuntu

--- nodejs.sh
+++ nodejs.sh
@@ -3,3 +3,9 @@
 # https://www.rosehosting.com/blog/install-npm-on-ubuntu-16-04/
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
+
+# http://blog.webbb.be/command-not-found-node-npm/
+npm config set prefix /usr/local
+# h265 video conversion scripts
+# https://github.com/FallingSnow/h265ize
+sudo npm install h265ize
