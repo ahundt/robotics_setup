b93065379db1e4b31caf358779e5b158e16cd438
Andrew Hundt <ATHundt@gmail.com>
2017-08-13 01:58:32 -0400

    python.sh pip3 workaround for Ubuntu 16.04

--- python.sh
+++ python.sh
@@ -20,12 +20,20 @@ case $OS in
         if [ "$DISTRIB_ID" == "Ubuntu" ]; then
             # install ubuntu python package
             sudo apt-get install -y python-setuptools python-dev build-essential
+
+            # workaround for pip 8.1.2 failing to upgrade on Ubuntu 16.04
+            # see https://github.com/pypa/pip/issues/3776
+            curl -o $HOME/Downloads/get-pip.py https://bootstrap.pypa.io/get-pip.py
+            sudo python3 $HOME/Downloads/get-pip.py
+
+            # commented below until workaround is no longer needed
             # python 2
-            sudo easy_install pip
-            pip install --upgrade virtualenv
+            # TODO(ahundt) this may be interfering with python3's pip3?
+            #sudo easy_install pip
+            #pip install --upgrade virtualenv
 
             # python 3
-            pip3 install --upgrade virtualenv
+            # pip3 install --upgrade virtualenv
         fi
     ;;
   'FreeBSD')
