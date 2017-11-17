a6caeb659c9989bd0353744f2e65df50529ad2f1
Andrew Hundt <ATHundt@gmail.com>
2017-11-16 19:25:39 -0500

    meshlab.sh added notes

--- meshlab.sh
+++ meshlab.sh
@@ -1,4 +1,19 @@
+# source: https://gist.github.com/phatblat/1713458
+# Save script's current directory
+DIR=$(pwd)
 
+set -e
+set -u
+set -x
+
+
+echo "############################"
+echo "# MeshLab -http://www.meshlab.net/"
+echo "############################"
+echo ""
+echo "The open source mesh processing system"
+echo ""
+echo "https://github.com/cnr-isti-vclab/meshlab"
 
 
 # os specific setup
@@ -17,3 +32,6 @@ case $OS in
         brew install meshlab
 	;;
 esac
+
+
+cd $DIR
