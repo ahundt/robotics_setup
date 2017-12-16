24ae894f283171f47c6fd364298996a8b149fc11
Andrew Hundt <ATHundt@gmail.com>
2017-12-15 19:58:25 -0500

    pip.sh checks if pip is already installed

--- pip.sh
+++ pip.sh
@@ -11,8 +11,15 @@ mkdir -p $HOME/Downloads
 mkdir -p $HOME/.local
 
 export PYTHONUSERBASE=$HOME/.local
+
 # workaround for pip 8.1.2 failing to upgrade on Ubuntu 16.04
 # see https://github.com/pypa/pip/issues/3776
+
 curl -o $HOME/Downloads/get-pip.py https://bootstrap.pypa.io/get-pip.py
-python2 $HOME/Downloads/get-pip.py --user
-python3 $HOME/Downloads/get-pip.py --user
+if  ! [ -x "$(command -v pip2)" ] ; then
+    python2 $HOME/Downloads/get-pip.py --user
+fi
+
+if  ! [ -x "$(command -v pip3)" ] ; then
+    python3 $HOME/Downloads/get-pip.py --user
+fi
