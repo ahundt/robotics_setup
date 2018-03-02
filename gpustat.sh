b605eafe3f6d83e3af483c3b7fb5991427e64faa
Andrew Hundt <ATHundt@gmail.com>
2018-03-02 12:16:47 -0500

    Create gpustat.sh

---
+++ gpustat.sh
@@ -0,0 +1,9 @@
+
+####################################
+# gpustat - see stats about your gpu
+# https://github.com/wookayin/gpustat
+#
+# to run:
+# watch --color -n1.0 gpustat --color
+
+pip install --user --upgrade gpustat
