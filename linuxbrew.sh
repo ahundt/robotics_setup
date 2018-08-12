6c6224636636d6277e1b87bcfe0eaed2584d17ee
Andrew Hundt <ATHundt@gmail.com>
2018-08-12 13:34:13 -0400

    linuxbrew.sh add script for raspberry pi workaround

--- linuxbrew.sh
+++ linuxbrew.sh
@@ -1,3 +1,10 @@
+# check if it is a raspberry pi, because we'll need a special ruby first
+if [ -x "$(command -v python)" ] ; then
+    R_PI=`python -c "import platform; print 'raspberrypi' in platform.uname()"`
 
+    if [ "$R_PI" = "True" ] ; then
+        yes | bash <(curl -s https://gist.githubusercontent.com/blacktm/8302741/raw/install_ruby_rpi.sh)
+    fi
+fi
 # install linuxbrew http://linuxbrew.sh
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
