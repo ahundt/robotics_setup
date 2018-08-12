1684b7d35c09a9aa9193839b70cdd4d10821ab80
Andrew Hundt <ATHundt@gmail.com>
2018-08-12 13:42:02 -0400

    add source for script

--- linuxbrew.sh
+++ linuxbrew.sh
@@ -3,6 +3,7 @@ if [ -x "$(command -v python)" ] ; then
     R_PI=`python -c "import platform; print 'raspberrypi' in platform.uname()"`
 
     if [ "$R_PI" = "True" ] ; then
+        # run ruby upgrade script. source: https://gist.github.com/blacktm/8302741
         yes | bash <(curl -s https://gist.githubusercontent.com/blacktm/8302741/raw/install_ruby_rpi.sh)
     fi
 fi
