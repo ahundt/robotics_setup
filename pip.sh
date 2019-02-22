0d75e28a75f9cdb2f7ab0dbf96d6866f42e340bb
Andrew Hundt <ATHundt@gmail.com>
2019-02-21 21:00:13 -0500

    pip.sh add comment for workaround to recent errror

--- pip.sh
+++ pip.sh
@@ -13,6 +13,28 @@ echo '#     sudo chmod g+wr -R ~/.local/'
 echo '#     sudo chown -R $USER ~/.cache/'
 echo '#     sudo chmod g+wr -R ~/.cache/'
 
+# https://github.com/pypa/pip/issues/5373
+# ### Another problem you might encounter
+# `sudo pip install --upgrade pip` using `9.0.1-3`.
+
+# ```shell
+# $ pip3 -V
+# Traceback (most recent call last):
+#   File "/usr/bin/pip3", line 7, in <module>
+#     from pip._internal import main
+# ModuleNotFoundError: No module named 'pip._internal'
+# ```
+
+# ### What (other) I've run:
+# ```
+# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+# python3 get-pip.py --force-reinstall
+
+# # Back to 9.0.1-3
+# pacman -Sy --force python-pip
+# pip uninstall pip
+# python3 get-pip.py
+# ```
 
 mkdir -p $HOME/Downloads
 mkdir -p $HOME/.local
